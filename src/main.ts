b5e4c908a51399e9b7c51c1d11479a66e6b21a0c
shaofan <akhuting@hotmail.com>
2021-05-18 17:31:55 +0800

    新加路由守护配置

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,15 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import router from "./router";
+import { setupRouter } from "./router";
 import store from "./store";
 
-import Antd from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.css';
+import Antd from "ant-design-vue";
+import "ant-design-vue/dist/antd.css";
 // import { Button, message } from 'ant-design-vue';
 
-createApp(App).use(Antd).use(store).use(router).mount("#app");
+const app = createApp(App);
+app.use(Antd);
+app.use(store);
+// 挂载路由
+setupRouter(app);
+app.mount("#app");
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,12 +1,13 @@
-import { createRouter, createWebHashHistory, RouteRecordRaw } from "vue-router";
+import { createRouter, createWebHashHistory, RouteRecordRaw, Router } from "vue-router";
 import Home from "../views/Home.vue";
 import Hello from "../views/Hello.vue";
+import { App } from "vue";
 
 const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Home",
-    component: Home,
+    component: Home
   },
   {
     path: "/about",
@@ -15,17 +16,39 @@ const routes: Array<RouteRecordRaw> = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/About.vue"),
-  },{
+      import(/* webpackChunkName: "about" */ "../views/About.vue")
+  }, {
     path: "/hello",
     name: "Hello",
-    component: Hello,
-  },
+    component: Hello
+  }
 ];
+
+const uap_url = process.env.UAP_URL;
+
+function createRouterGuards(router: Router) {
+  router.beforeEach((to, from, next) => {
+    // 从localstorage中获取token
+    const token = null;
+    if (token) {
+      next();
+    } else {
+      window.location.href = uap_url;
+    }
+  });
+
+  router.onError((error) => {
+    console.log(error, "路由错误");
+  });
+}
 
 const router = createRouter({
   history: createWebHashHistory(),
-  routes,
+  routes
 });
 
-export default router;
+export function setupRouter(app: App) {
+  app.use(router);
+  // 创建路由守卫
+  createRouterGuards(router);
+}
